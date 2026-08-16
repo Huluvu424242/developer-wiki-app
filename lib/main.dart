ed9358c46a13fc71147fa20680b0f5db6523b7f5
Huluvu424242 <huluvu424242@gmail.com>
2026-08-16 17:53:23 +0200

    Add lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'screens/source_form_screen.dart';
+void main()=>runApp(const WikiSourceApp());
+class WikiSourceApp extends StatelessWidget {const WikiSourceApp({super.key});
+ @override Widget build(BuildContext context)=>MaterialApp(debugShowCheckedModeBanner:false,title:'Developer Wiki Quellen',theme:ThemeData(colorSchemeSeed:Colors.indigo,useMaterial3:true),home:const SourceFormScreen());}
