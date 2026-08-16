fa2d866aa53b46da12aed9535cf9dafc08c4be25
Huluvu424242 <huluvu424242@gmail.com>
2026-08-16 17:53:15 +0200

    Add android/app/build.gradle.kts

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android"); id("dev.flutter.flutter-gradle-plugin") }
+
+android {
+    namespace = "de.huluvu.developer_wiki_source_capture"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "de.huluvu.developer_wiki_source_capture"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
+
+flutter { source = "../.." }
