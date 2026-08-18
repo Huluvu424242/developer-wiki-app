69efc110fe8675fba863505abc2949188c1786c5
Huluvu424242 <huluvu424242@gmail.com>
2026-08-18 22:58:15 +0200

    Fix Kotlin JVM target compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,22 @@
-plugins { id("com.android.application"); id("org.jetbrains.kotlin.android"); id("dev.flutter.flutter-gradle-plugin") }
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
 
 android {
     namespace = "de.huluvu.developer_wiki_source_capture"
     compileSdk = 35
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     defaultConfig {
         applicationId = "de.huluvu.developer_wiki_source_capture"
         minSdk = 23
@@ -12,4 +26,6 @@ android {
     }
 }
 
-flutter { source = "../.." }
+flutter {
+    source = "../.."
+}
