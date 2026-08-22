b63478bc7998812a3030adfc5935a0005ea9486a
Huluvu424242 <huluvu424242@gmail.com>
2026-08-22 18:19:49 +0200

    Upgrade Android Gradle Plugin to 8.13.2

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
 }
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.7.0" apply false
+    id("com.android.application") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false
 }
 rootProject.name = "developer_wiki_source_capture"
