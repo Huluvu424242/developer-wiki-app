86b6f1b2c40b1392c7af691880af6d681852c61a
Huluvu424242 <huluvu424242@gmail.com>
2026-08-22 18:54:46 +0200

    Update Kotlin Gradle Plugin to 2.2.20

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -10,7 +10,7 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.13.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
 rootProject.name = "developer_wiki_source_capture"
 include(":app")
